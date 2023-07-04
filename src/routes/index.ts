5b868347fa85ce2d7337b8e2f4ce64496e9990fa
Kévin Guieba <kevin.guieba@gmail.com>
2023-07-04 11:55:41 -0400

    Correction du nom de l'api dans l'index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  return res.json({ message: "Bienvenue sur l'API GC-ADMIN" });
+  return res.json({ message: "Bienvenue sur l'API KASA - backend" });
 });
 
 export default router;
